4dc34da02ce82f4f4fe5c85de8f102b6b2585a9d
Anna Conti <anna.conti@thinkopen.it>
2019-10-30 12:01:48 +0100

    refactoring

--- p1overa_App/index.js
+++
@@ -1,6 +0,0 @@
-let key = 'd999a578d6d8fff8855a42b1525503a5';
-
-//date to be continued
-const options = { weekday: 'long', day: 'numeric', month: 'short' };
-const today = new Date();
-document.getElementById('date').innerHTML = today;
